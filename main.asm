fb663647c74c241ba1f1562ee0b4690a6ee64086
Ivan <boss.marzan@bk.ru>
2022-07-11 18:14:44 +0300

    Created configuration the interrupt on INT0 (PD1)

--- main.asm
+++ main.asm
@@ -5,20 +5,31 @@
     .org 0
     rjmp Reset
     Reset:
-    ldi temp, 0b00000001
-    out DDRD, temp
+    ldi temp, 0b00000010
+    out PORTD, temp
+    
     Program:
-    ldi temp, 0b00000001
-    out PORTD, temp
-    rcall Delay
-    ldi temp, 0
-    out PORTD, temp
-    rcall Delay
+    
     rjmp Program
 
-    Delay:
-    ldi temp, 0xff
-    LoopDelay:
-    dec temp
-    brne LoopDelay
-    ret
+    ConfigureInterruptINT0:
+    push temp
+    ldi temp, 0b10000000
+    out SREG, temp  ;enable global interrupts
+    ldi temp, 0b00000011
+    out EICRA, temp ;enable interrupt INT0 by the rising power 
+    ldi temp, 0b00000001
+    out EIMSK, temp
+    pop temp
+
+    INT0:
+    push temp
+    sbic PIND, 1 
+    ldi temp, 0b00000010
+    out PORTD, temp
+    
+    
+
+
+    
+
